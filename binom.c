8a9aaebe7faca2dc38b945bdd8aba1117599282b
orthoplex <inf18117@lehre.dhbw-stuttgart.de>
2021-10-10 00:01:52 +0200

    performance improvements

--- binom.c
+++ binom.c
@@ -10,23 +10,25 @@ int main(int argc, char *argv[]) {
     const int EXP = 665829;
     const int N = atoi(argv[1]);
 
-    mpz_t s;
-    mpz_init(s);
-    mpz_set_ui(s, 0);
-    mpz_t binom;
-    mpz_init(binom);
+    mpz_t s, binom, rem;
+    mpz_inits(s, binom, rem, NULL);
     mpz_set_ui(binom, 1);
-    mpz_t rem;
-    mpz_init(rem);
     long dsum = 0;
 
     for (int k = 0; k <= N; k++) {
         mpz_add(s, s, binom); 
         
-        for (int j = 0; j < EXP && mpz_sgn(s) != 0; j++) {
-            mpz_fdiv_qr_ui(s, rem, s, 10);
-            long lrem = mpz_get_ui(rem);
-            dsum += lrem;
+        for (int j = 0; j < EXP && mpz_sgn(s) != 0;) {
+            if (j + 18 <= EXP) {
+                mpz_fdiv_qr_ui(s, rem, s, 1000000000000000000);
+                for (long lrem = mpz_get_ui(rem); lrem; lrem /= 10)
+                    dsum += lrem % 10;
+                j += 18;
+            } else {
+                mpz_fdiv_qr_ui(s, rem, s, 10);
+                dsum += mpz_get_ui(rem);
+                j += 1;
+            }
         }
 
         mpz_mul_ui(binom, binom, 3 * (N - k));
@@ -38,9 +40,7 @@ int main(int argc, char *argv[]) {
         dsum += mpz_get_ui(rem);
     }
 
-    mpz_clear(s);
-    mpz_clear(binom);
-    mpz_clear(rem);
+    mpz_clears(s, binom, rem, NULL);
 
     printf("Digital sum of (3*10^%d+1)^%d: %ld\n", EXP, N, dsum);
 
