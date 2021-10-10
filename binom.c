508e460880dda6aa9ad33b512aa5977df76b4d39
orthoplex <inf18117@lehre.dhbw-stuttgart.de>
2021-10-10 13:45:07 +0200

    string conversion improvement

--- binom.c
+++ binom.c
@@ -7,31 +7,23 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "usage: %s exponent\n", argv[0]);
         exit(EXIT_FAILURE);
     }
-    const int EXP = 665829;
-    const int N = atoi(argv[1]);
 
-    mpz_t s, binom, rem;
-    mpz_inits(s, binom, rem, NULL);
-    mpz_set_ui(binom, 1);
+    int EXP = 665829;
+    int n = atoi(argv[1]);
+    char remstr[EXP + 1];
     long dsum = 0;
+    mpz_t s, binom, mod, rem;
+    mpz_inits(s, binom, mod, rem, NULL);
+    mpz_set_ui(binom, 1);
+    mpz_ui_pow_ui(mod, 10, EXP);
 
-    for (int k = 0; k <= N; k++) {
+    for (int k = 0; k <= n; k++) {
         mpz_add(s, s, binom); 
-        
-        for (int j = 0; j < EXP && mpz_sgn(s) != 0;) {
-            if (j + 18 <= EXP) {
-                mpz_fdiv_qr_ui(s, rem, s, 1000000000000000000);
-                for (long lrem = mpz_get_ui(rem); lrem; lrem /= 10)
-                    dsum += lrem % 10;
-                j += 18;
-            } else {
-                mpz_fdiv_qr_ui(s, rem, s, 10);
-                dsum += mpz_get_ui(rem);
-                j += 1;
-            }
-        }
-
-        mpz_mul_ui(binom, binom, 3 * (N - k));
+        mpz_fdiv_qr(s, rem, s, mod);
+        mpz_get_str(remstr, 10, rem);
+        for (int i = 0; remstr[i]; i++)
+            dsum += remstr[i] - '0';
+        mpz_mul_ui(binom, binom, 3 * (n - k));
         mpz_fdiv_q_ui(binom, binom, k + 1);
     }
 
@@ -40,12 +32,12 @@ int main(int argc, char *argv[]) {
         dsum += mpz_get_ui(rem);
     }
 
-    mpz_clears(s, binom, rem, NULL);
+    mpz_clears(s, binom, mod, rem, NULL);
 
-    printf("Digital sum of (3*10^%d+1)^%d: %ld\n", EXP, N, dsum);
+    printf("Digital sum of (3*10^%d+1)^%d: %ld\n", EXP, n, dsum);
 
-    if ((dsum - N * 4) % 7 == 0)
-        printf("(3*10^%d+1)^%d*10^%ld is Smith.\n", EXP, N, (dsum - N * 4) / 7);
+    if ((dsum - n * 4) % 7 == 0)
+        printf("(3*10^%d+1)^%d*10^%ld is Smith.\n", EXP, n, (dsum - n * 4) / 7);
     
     return EXIT_SUCCESS;
 }
